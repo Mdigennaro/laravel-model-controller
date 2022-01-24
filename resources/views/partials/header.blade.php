234fae783369431ebfac29de1495db3420e21e2b
Mdigennaro <michele.digennaro719@gmail.com>
2022-01-24 18:01:48 +0100

    aggiunta route

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+  <div class="container text-center">
+    
+    <h1 class="display-1">Movie</h1>
+
+    <ul class="nav d-flex justify-content-center display-4">
+      <li class="nav-item">
+        <a class="nav-link active" href="{{route('home')}}">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{route('about')}}">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{route('contact')}}">Contact</a>
+      </li>
+    </ul>
+
+  </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contacts');
+})->name('contact');
